577cd0aadb82e421aaf0facd7d06e66eec603b28
Pietro Govoni <govoni@lxplus0104.cern.ch>
2014-01-17 11:33:39 +0100

    added some infoes to the generation, first plots of resolution

--- scripts/plotResolution.C
+++ scripts/plotResolution.C
@@ -6,20 +6,6 @@ TRandom3 * gRandom ;
 
 
 // LY = photons per MeV in the fibre
-float resolution (Float_t * x, Float_t * par)
-{
-  if (x[0] == 0) return 0. ;
-  float result = par[0] * par[0] / (x * x) + // noise
-                 par[1] * par[1] / x +       // stochastic
-                 par[2] ;                    // constant
-  return sqrt (result) ;                 
-}
-
-
-// ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
-
-
-// LY = photons per MeV in the fibre
 float smearEnergy (float energy, float LY = 10000, float acceptance = 0.1, float efficiency = 0.1)
 {
   LY *= 1000 ; // 1000 MeV = 1 GeV
@@ -103,26 +89,6 @@ void plotResolution (TString inputFile)
         h_sigmaEoverE.Fill (sigmaEoverE) ;
         h_sigmaEoverE_vs_Ebeam.Fill (beamEnergy, sigmaEoverE) ;
      }
-
-   // analysis of the distrbutions
-  sigmaEoverE_vs_Ebeam->SetStats (0)
-  sigmaEoverE_vs_Ebeam->Draw ("colz")
-  sigmaEoverE_vs_Ebeam->FitSlicesY ()
-  gDirectory->ls ()
-  sigmaEoverE_vs_Ebeam_1->SetMarkerStyle (8)
-  sigmaEoverE_vs_Ebeam_1->SetMarkerColor (kRed)
-  sigmaEoverE_vs_Ebeam_1->Draw ("same")
-  TCanvas c2
-  sigmaEoverE_vs_Ebeam_2->Draw ()
-  sigmaEoverE_vs_Ebeam_2->SetStats (0)
-  sigmaEoverE_vs_Ebeam_2->SetMarkerStyle (4)
-  sigmaEoverE_vs_Ebeam_2->Draw ()
-  TF1 * resolution = new TF1 ("resolution", "[0] / x + [1] / TMath::Sqrt(x) + [2]" ,0, 200.) ;
-  sigmaEoverE_vs_Ebeam_2->Fit (resolution)
-  sigmaEoverE_vs_Ebeam_2->SetStats (1)
-  sigmaEoverE_vs_Ebeam_2->Draw ()
-   
-
 
    TFile outfile ("plotResolution.root", "recreate") ;
    outfile.cd () ;
