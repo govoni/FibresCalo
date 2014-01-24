05b66b2adb1905bce3f22377394555fc16dcee42
Pietro Govoni <govoni@lxplus0001.cern.ch>
2014-01-24 10:44:27 +0100

    bugfix

--- scripts/getShowerProperties.C
+++ scripts/getShowerProperties.C
@@ -1,12 +1,11 @@
-
 int getShowerProperties (TString inputFileName)
 {
 
-  TH1F * h_E_vs_R = new TH1F ("h_E_vs_R", "h_E_vs_R", 200, 0., 20.) ;
-  TH1F * h_E_vs_L = new TH1F ("h_E_vs_L", "h_E_vs_L", 3000, 0., 300.) ;
-
   TFile f (inputFileName) ;
   TNtuple * stepDeposits = (TNtuple *) f.Get ("stepDeposits") ;
+
+  TH1F * h_E_vs_R = new TH1F ("h_E_vs_R", "h_E_vs_R", 200, 0., 20.) ;
+  TH1F * h_E_vs_L = new TH1F ("h_E_vs_L", "h_E_vs_L", 3000, 0., 300.) ;
 
   Float_t x, y, z, E ;
   stepDeposits->SetBranchAddress ("x", &x) ;
@@ -26,17 +25,17 @@ int getShowerProperties (TString inputFileName)
       if (i % 100000 == 0) cout << "event " << i << endl ;
       stepDeposits->GetEntry (i) ;
       TVector3 point (x, y, z) ;
-      double projection = direction->Dot (point) ;
-      double distance = sqrt (point->Mag2 () - projection * projection) ;
+      double projection = direction.Dot (point) ;
+      double distance = sqrt (point.Mag2 () - projection * projection) ;
       h_E_vs_R->Fill (distance, E) ;
       h_E_vs_L->Fill (projection, E) ;
     }
 
-  TFile f_out (TString ("summ_") + inputFileName, "recreate") ;
-  f_out.cd () ;
-  h_E_vs_R->Draw () ;
-  h_E_vs_L->Draw () ;
-  f_out.Close () ;
+  TFile * f_out = new TFile (TString ("summ_") + inputFileName, "recreate") ;
+  f_out->cd () ;
+  h_E_vs_R->Write () ;
+  h_E_vs_L->Write () ;
+  f_out->Close () ;
 
   return 0 ;
   
