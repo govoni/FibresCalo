bd16b4490b4de6f49415b4814e371b7a04bd8f95
Pietro Govoni <govoni@lxplus0048.cern.ch>
2014-02-01 17:10:00 +0100

    improving submission script

--- submit2.py
+++ submit2.py
@@ -23,13 +23,14 @@ def replaceParameterInFile (inputFile, outputFile, replacementTag, replacementVa
 # ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
 
-def prepareConfigs (energy, outputFolder, bfield, events) :
+def prepareConfigs (energy, outputFolder, bfield, events, material) :
     GPSfileName = outputFolder + '.mac'
     replaceParameterInFile ('template.mac', GPSfileName, 'ENERGY', energy)
     replaceParameterInFile (GPSfileName, GPSfileName, 'EVENTS', events)
     configFileName = outputFolder + '.cfg'
     replaceParameterInFile ('template.cfg', configFileName, 'GPSFILE', GPSfileName)
     replaceParameterInFile (configFileName, configFileName, 'BFIELD', bfield)
+    replaceParameterInFile (configFileName, configFileName, 'MATERIAL', material)
     return configFileName
 
 
@@ -52,7 +53,7 @@ def submitJob (jobID, outputFolder, configFileName, queue):
     f.write ('cmsStage out_' + filename + '.root /store/user/govoni/upgrade/' + outputFolder + '\n')
     f.close ()
     getstatusoutput ('chmod 755 ' + filename)
-    getstatusoutput ('bsub -q ' + queue + ' -J ' + jobID + ' ' + filename)
+    getstatusoutput ('bsub -q ' + queue + ' -J ' + filename + ' ' + filename)
 
 
 # ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
@@ -61,18 +62,29 @@ def submitJob (jobID, outputFolder, configFileName, queue):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser (description = 'boh')
-    parser.add_argument('-q', '--queue'  , default= '1nd',  help='batch queue (1nd)')
-    parser.add_argument('-j', '--jobsNum', default= 1,      type=int, help='number of jobs (1)')
-    parser.add_argument('-e', '--energy' , default= '50',   help='beam energy (50)')
-    parser.add_argument('-o', '--outTag' , default= 'fix_', help='outfolder tag base name (fix_)')
-    parser.add_argument('-b', '--bfield' , default= '0',    help='magnetic filed value (0)')
-    parser.add_argument('-n', '--events' , default= '10000',help='number of generated events (10000)')
+    parser.add_argument('-q', '--queue'    , default= '1nd',   help='batch queue (1nd)')
+    parser.add_argument('-j', '--jobsNum'  , default= 1,       type=int, help='number of jobs (1)')
+    parser.add_argument('-e', '--energy'   , default= '50',    help='beam energy (50)')
+    parser.add_argument('-o', '--outTag'   , default= 'fix_',  help='outfolder tag base name (fix_)')
+    parser.add_argument('-b', '--bfield'   , default= '0',     help='magnetic filed value (0)')
+    parser.add_argument('-n', '--events'   , default= '10000', help='number of generated events (10000)')
+    parser.add_argument('-m', '--material' , default= 'W',     help='absorber material [W, Pb] (W)')
     
     args = parser.parse_args ()
 
+    material = '-1'
+    if args.material == 'W' : material = '2'
+    elif args.material == 'Pb' : material = '3'
+    else:
+        print 'the absorber material', args.material, 'is not implemented, quitting'
+        sys.exit (1)
+
     outputFolder = args.outTag + args.energy 
-    configFileName = prepareConfigs (args.energy, outputFolder, args.bfield, args.events)
+    configFileName = prepareConfigs (args.energy, outputFolder, args.bfield, args.events, material)
 
     print 'submitting', args.jobsNum, 'jobs to queue', args.queue
     for i in range (0,  args.jobsNum):
-        submitJob (str (i), outputFolder, configFileName, args.queue)    
+        submitJob (str (i), outputFolder, configFileName, args.queue)  
+        
+        
+      
